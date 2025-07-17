de4d9f4c070936a0d1040f6aab527d1d018a02b5
Robert Reinold <rreinold@gmail.com>
2025-07-17 03:47:41 +0000

    Remove out
    This reverts commit 536111bf079b00ef2f267b13f4a6edb8660f6ceb.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  /* config options here */
 };
 
-module.exports = nextConfig
+export default nextConfig;
