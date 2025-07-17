f51914b95a7fd125007734c26fd5b3ea664e83be
Robert Reinold <rreinold@gmail.com>
2025-07-17 03:54:35 +0000

    add prefix

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/your-repository-name', // Replace with your actual repo name
+  assetPrefix: '/your-repository-name',
   images: {
     unoptimized: true
   }
 }
+
+module.exports = nextConfig
 
 export default nextConfig;
